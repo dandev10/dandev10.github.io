5eff8d3c3f15d2ad07e0211773236b382627ac6b
THEspager <danilagon@icloud.com>
2021-10-21 18:23:59 +0200

    my last uptade

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log("hello Danila");
+console.log("heieeieieiei my pc goes brrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr");
 
 var x = 1 + 2 * 3;
 
